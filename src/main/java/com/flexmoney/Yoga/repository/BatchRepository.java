26444081cc507ad9adf00d08ec0fffa6d0d990dd
mastersibin <sibinjordaan@gmail.com>
2022-12-13 10:46:23 +0530

    added repo methods

--- src/main/java/com/flexmoney/Yoga/repository/BatchRepository.java
+++ src/main/java/com/flexmoney/Yoga/repository/BatchRepository.java
@@ -1,7 +1,9 @@
 package com.flexmoney.Yoga.repository;
 
 import com.flexmoney.Yoga.entities.Batch;
+import com.flexmoney.Yoga.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BatchRepository extends CrudRepository<Batch, Long> {
+    Batch findByUser(User user);
 }
--- src/main/java/com/flexmoney/Yoga/repository/UserBatchRepository.java
+++ src/main/java/com/flexmoney/Yoga/repository/UserBatchRepository.java
@@ -1,7 +1,9 @@
 package com.flexmoney.Yoga.repository;
 
+import com.flexmoney.Yoga.entities.User;
 import com.flexmoney.Yoga.entities.UserBatch;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserBatchRepository extends CrudRepository<UserBatch, Long> {
+    UserBatch findByUser(User user);
 }
