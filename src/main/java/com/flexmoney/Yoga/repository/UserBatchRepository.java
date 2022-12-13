7671e649d4c380b2a7d268a9973c16b9911f08a9
mastersibin <sibinjordaan@gmail.com>
2022-12-13 13:58:51 +0530

    query for cron

--- src/main/java/com/flexmoney/Yoga/repository/UserBatchRepository.java
+++ src/main/java/com/flexmoney/Yoga/repository/UserBatchRepository.java
@@ -2,8 +2,15 @@ package com.flexmoney.Yoga.repository;
 
 import com.flexmoney.Yoga.entities.User;
 import com.flexmoney.Yoga.entities.UserBatch;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserBatchRepository extends CrudRepository<UserBatch, Long> {
     UserBatch findByUser(User user);
+    @Transactional
+    @Modifying
+    @Query(value = "update UserBatch set batch = nextBatch, nextBatch = NULL where nextBatch is not NULL")
+    void setNextBatch();
 }
