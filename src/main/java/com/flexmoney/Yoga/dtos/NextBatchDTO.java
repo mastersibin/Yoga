362b4a88a7c8bfdf721fc1e88a5598cdd290c897
mastersibin <sibinjordaan@gmail.com>
2022-12-13 10:47:41 +0530

    added dtos for allowed and setNextBatch endpoint

---
+++ src/main/java/com/flexmoney/Yoga/dtos/NextBatchDTO.java
@@ -0,0 +1,13 @@
+package com.flexmoney.Yoga.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NextBatchDTO {
+    String name;
+    Long batch_id;
+}
